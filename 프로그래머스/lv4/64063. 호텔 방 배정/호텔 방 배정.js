5ddea416684b1aec17d784fe7f98b148908c527e
Yong-Woo, Lee <110705019+wooleejaan@users.noreply.github.com>
2023-04-10 21:57:14 +0900

    [level 4] Title: 호텔 방 배정, Time: 268.16 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/lv4/64063. 호텔 방 배정/호텔 방 배정.js
+++ 프로그래머스/lv4/64063. 호텔 방 배정/호텔 방 배정.js
@@ -1,21 +1,4 @@
-function findEmpty(number, rooms){
-    if(!rooms.has(number)){
-        rooms.set(number, number + 1)
-        return number 
-    }
-    
-    let next = findEmpty(rooms.get(number), rooms)
-    rooms.set(number, next + 1)
-    return next 
-}
-
 function solution(k, room_number) {
     var answer = [];
-    const rooms = new Map() 
-    
-    room_number.forEach((rn) => {
-        answer.push(findEmpty(rn, rooms))
-    })
-    
     return answer;
 }
