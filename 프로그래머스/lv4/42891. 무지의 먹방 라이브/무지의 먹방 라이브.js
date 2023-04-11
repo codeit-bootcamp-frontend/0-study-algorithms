1d6097304c91c8428f32f2b627593d906ff0d17d
Yong-Woo, Lee <110705019+wooleejaan@users.noreply.github.com>
2023-04-11 20:38:54 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 316.98 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/lv4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.js
+++ 프로그래머스/lv4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.js
@@ -1,25 +1,27 @@
 function solution(food_times, k) {
-  const len = food_times.length;
-
-  let foods = food_times
-    .map((time, index) => {
-      return { index: index + 1, time };
-    })
-    .sort((a, b) => a.time - b.time);
-
-  let previous = 0;
-  for (let i = 0; i < len; i++) {
-    const currentTime = foods[i].time;
-    const remainFoodsLen = len - i;
-    const eatTime = (currentTime - previous) * remainFoodsLen;
-    previous = currentTime;
-
-    if (k < eatTime) {
-      foods = foods.slice(i).sort((a, b) => a.index - b.index);
-      return foods[k % remainFoodsLen].index;
+    let count=0;
+    let queue=food_times.map((a,b)=>[a,b]).sort((a,b)=>a[0]-b[0]);
+    const total = food_times.reduce((a,b)=>{return a+b},0);
+    if(total<=k) return -1;
+    
+    let idx=0;
+    while(queue.length-idx){
+        let sum=queue[idx][0]-count;
+        if(k-(queue.length-idx)<0) break;
+        while(k-(queue.length-idx)*sum<0)
+            sum--;
+        
+        k-=(queue.length-idx)*sum;
+        count+=sum;
+        while(true){
+            if(queue[idx][0]-count===0){
+                queue[idx][0]=0;
+                idx++;
+            }else{
+                break;
+            }
+        } 
     }
-    k -= eatTime;
-  }
-
-  return -1;
+    queue=queue.filter((a,b)=>a[0]>0).sort((a,b)=>a[1]-b[1]);
+    return queue[k][1]+1;
 }
