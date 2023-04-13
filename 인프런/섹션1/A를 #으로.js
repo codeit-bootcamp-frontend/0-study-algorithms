2f61cf19fbc9c6834270f14144f50086ec1e93c8
임병욱 <bwlimtony@gmail.com>
2023-04-13 11:26:35 +0900

    [IFL] 가운데 문자 출력  / 섹션1 /  1분

--- 인프런/섹션1/A를 #으로.js
+++ 인프런/섹션1/A를 #으로.js
@@ -17,5 +17,4 @@ function solution(s) {
 let str = 'BANANA';
 console.log(solution(str));
 
-// str.replace(regexp|substr, newSubstr|function) 형식으로 첫번째 파라미터에 정규표현식을 넣을 수도 있다
-// 리턴값으로는 어떤 패턴에 일치하는 일부 또는 모든 부분이 교체된 새로운 문자열
+// ㅇ// 리턴값으로는 어떤 패턴에 일치하는 일부 또는 모든 부분이 교체된 새로운 문자열
