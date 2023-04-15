f3494b29aa7df6c53f7830e694158cccf78e9115
임병욱 <bwlimtony@gmail.com>
2023-04-15 16:57:11 +0900

    [IFL] 3. 크레인 인형뽑기 / 섹션6 /  1분

---
+++ 인프런/섹션6/3. 크레인 인형뽑기.js
@@ -0,0 +1,30 @@
+function solution(board, moves) {
+  const stack = [];
+  let answer = 0;
+  moves.forEach((move) => {
+    for (let i = 0; i < board.length; i++) {
+      let temp = board[i][move - 1];
+      if (board[i][move - 1] !== 0) {
+        if (temp === stack[stack.length - 1]) {
+          answer++;
+          stack.pop();
+        } else {
+          stack.push(board[i][move - 1]);
+          board[i][move - 1] = 0;
+        }
+      }
+    }
+  });
+  return answer;
+}
+
+let a = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 3],
+  [0, 2, 5, 0, 1],
+  [4, 2, 4, 4, 2],
+  [3, 5, 1, 3, 1],
+];
+
+let b = [1, 5, 3, 5, 1, 2, 1, 4];
+console.log(solution(a, b));
