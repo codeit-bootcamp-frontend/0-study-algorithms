7bcdb0b663b8dc49d32c9476b2a894eb995c604c
임병욱 <bwlimtony@gmail.com>
2023-04-18 15:32:43 +0900

    [IFL] 10. 이분검색 / 섹션7 /  1분

---
+++ 인프런/섹션7/10. 이분검색.js
@@ -0,0 +1,21 @@
+function solution(target, arr) {
+  let answer = 0;
+  arr.sort((a, b) => a - b);
+  let left = 0;
+  let right = arr.length - 1;
+  while (left <= right) {
+    let mid = parseInt((left + right) / 2);
+    if (arr[mid] === target) {
+      answer = mid;
+      break;
+    } else if (arr[mid] > target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return answer;
+}
+
+let arr = [23, 87, 65, 12, 57, 32, 99, 81];
+console.log(solution(32, arr));
