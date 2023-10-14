170a687511eb2b94222e68336a224bed6956bc99
drizzle96 <125791542+drizzle96@users.noreply.github.com>
2023-10-14 15:19:26 +0900

    [level 2] Title: 구명보트, Time: 8.88 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,7 +1,6 @@
 def solution(people, limit):
     answer = 0
     people.sort()
-    print(people)
     left, right = 0, len(people) - 1
     while (left <= right):
         if people[left] + people[right] > limit:
