47f2c036f837175324188d5c7a0a08781ce7de87
drizzle96 <125791542+drizzle96@users.noreply.github.com>
2023-10-20 13:23:23 +0900

    [level 2] Title: 조이스틱, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/README.md
+++ 프로그래머스/2/42860. 조이스틱/README.md
@@ -1,10 +1,10 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860#) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.02 ms
+메모리: 10.4 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 5일 13:16:18
+2023년 10월 5일 13:23:22
 
 ### 문제 설명
 
--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,3 +1,4 @@
+# s1을 s2로 조작할 때 필요한 횟수
 def cal_num_mani_str(s1, s2):
     s1_ord = ord(s1)
     s2_ord = ord(s2)
@@ -11,6 +12,7 @@ def solution(name):
     move_pos_num = length - 1
     change_str_num = 0
     
+    # a가 들어있는 위치를 리스트로 도출
     is_before_a = False
     for i, s in enumerate(name):
         if i == 0:
@@ -30,7 +32,12 @@ def solution(name):
             
     if a_list and len(a_list[-1]) == 1:
         a_list[-1].append(-1)
-    print(a_list)
+    
+    # 아래 3 경우의 이동 횟수 중 최소를 계산
+    # 1. a 상관 없이 한 방향으로 계속 이동
+    # 2. 특정 a 덩어리를 통과하지 않을 때
+    # 2-1. a 덩어리 왼쪽 이동 후 오른쪽으로 돌아가기
+    # 2-2. a 덩어리 오른 쪽 이동 후 왼쪽으로 돌아가기
     for s, e in a_list:
         if s == 1 and e == -1:
             move_pos_num = 0
@@ -44,7 +51,8 @@ def solution(name):
             right_dir = (length - e - 1) + (length - (e - s) - 2)
             move_pos_num = min(move_pos_num, min(left_dir, right_dir))
 
+    # 각 문자를 A에서 조작할 때 필요한 횟수의 합 계산
     for s in name:
         change_str_num += cal_num_mani_str("A", s)
-    
+    # 조작할 문자로 이동하는 횟수 + 각 문자 조작 횟수
     return move_pos_num + change_str_num
