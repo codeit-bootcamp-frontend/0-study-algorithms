98c70b96348c46407df059e49c732d40fecf8f8c
drizzle96 <125791542+drizzle96@users.noreply.github.com>
2023-10-22 00:04:40 +0900

    [level 2] Title: 숫자 변환하기, Time: 1.07 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -2,7 +2,7 @@ from collections import deque
 
 def solution(x, y, n):
     answer = 0
-    visited = {}
+    num_set = set()
     queue = deque()
     queue.append((x, 0)) # 현재 숫자, 연산 횟수
     while queue:
@@ -11,14 +11,14 @@ def solution(x, y, n):
             return iter_num
         tri, double, plus = num * 3, num * 2, num + n
             
-        if tri <= y and tri not in visited:
+        if tri <= y and tri not in num_set:
             queue.append((tri, iter_num + 1))
-            visited[tri] = True
-        if double <= y and double not in visited:
+            num_set.add(tri)
+        if double <= y and double not in num_set:
             queue.append((double, iter_num + 1))
-            visited[double] = True
-        if plus <= y and plus not in visited:
+            num_set.add(double)
+        if plus <= y and plus not in num_set:
             queue.append((plus, iter_num + 1))
-            visited[plus] = True
+            num_set.add(plus)
 
     return -1
