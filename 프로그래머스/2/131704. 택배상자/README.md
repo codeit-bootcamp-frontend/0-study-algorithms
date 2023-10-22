4696ed561f4457eb1d7d5f2241f0cee4c13b4c3c
drizzle96 <125791542+drizzle96@users.noreply.github.com>
2023-10-22 16:01:59 +0900

    [level 2] Title: 택배상자, Time: 272.12 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/README.md
+++ 프로그래머스/2/131704. 택배상자/README.md
@@ -1,10 +1,10 @@
 # [level 2] 택배상자 - 131704 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131704#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131704) 
 
 ### 성능 요약
 
-메모리: 53 MB, 시간: 271.26 ms
+메모리: 52.9 MB, 시간: 272.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 0일 15:49:52
+2023년 10월 0일 16:1:58
 
 ### 문제 설명
 
--- 프로그래머스/2/131704. 택배상자/택배상자.py
+++ 프로그래머스/2/131704. 택배상자/택배상자.py
@@ -1,29 +1,12 @@
 def solution(order):
-    answer = 0
-    assist_container = []
-    order_idx = 0
+    stack_list = []
+    idx = 0
     for i in range(len(order)):
-        box_num = i + 1
-        # 보조 컨테이너 박스에서 일치하는 만큼 실어주기
-        while assist_container and assist_container[-1] == order[order_idx]:
-            answer += 1
-            order_idx += 1
-            assist_container.pop()
-        # 컨테이너 박스와 필요 박스가 일치할 때
-        if box_num == order[order_idx]:
-            answer += 1
-            order_idx += 1
-        # 둘 다 일치하지 않을 때
-        else:
-            if box_num < order[order_idx]:
-                assist_container.append(box_num)
+        stack_list.append(i + 1)
+        while stack_list:
+            if stack_list[-1] == order[idx]:
+                idx += 1
+                stack_list.pop()
             else:
                 break
-    
-    # 보조 컨테이너 한 번 더 확인
-    while (assist_container and assist_container[-1] == order[order_idx]):
-        answer += 1
-        order_idx += 1
-        assist_container.pop()
-        
-    return answer
+    return idx
