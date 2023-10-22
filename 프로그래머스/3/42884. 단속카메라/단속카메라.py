53984c0193dba8b56083dd49dfe7a8d4095deffe
drizzle96 <125791542+drizzle96@users.noreply.github.com>
2023-10-22 17:54:00 +0900

    [level 3] Title: 단속카메라, Time: 1.06 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,21 +1,11 @@
 def solution(routes):
     answer = 0
-    routes.sort(key=lambda x: x[0])
-    idx = 0
+    last_search_pos = -30001
+    routes.sort(key=lambda x: x[1])
     
-    while idx < len(routes) - 1:
-        answer += 1
-        start = routes[idx][0]
-        end = routes[idx][1]
-        search = start
-        while search <= end:
-            while idx < len(routes) - 1 and routes[idx + 1][0] <= search:
-                end = min(end, routes[idx + 1][1])
-                idx += 1
-            search += 1
-        idx += 1
-        
-    if idx == len(routes) - 1:
-        answer += 1
+    for route in routes:
+        if last_search_pos < route[0]:
+            answer += 1
+            last_search_pos = route[1]
     
     return answer
