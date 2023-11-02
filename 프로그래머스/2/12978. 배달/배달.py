8403f4fe62dc300272ab28f29551115d5b45dad5
drizzle96 <125791542+drizzle96@users.noreply.github.com>
2023-11-02 11:37:38 +0900

    [level 2] Title: 배달, Time: 0.07 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -2,6 +2,8 @@ import heapq
 
 def solution(N, road, K):
     # 다익스트라 알고리즘 연습
+    # 우선순위 큐는 데이터의 첫 번째 요소의 최솟값을 기준점으로 잡는다.
+    # 따라서 우선순위 큐에서 가장 가까운 노드를 꺼내기 위해 (가중치, 노드)의 순서로 되어있는 데이터를 만들어 넣어줘야 한다.
     INF = 1e8
     
     # 그래프, 거리 리스트 생성
@@ -11,15 +13,16 @@ def solution(N, road, K):
         graph[s].append((e, w))
         graph[e].append((s, w))
     distance = [INF] * (N+1)
-    distance[1] = 0
     
     # 힙 초기화
     queue = []
-    heapq.heappush(queue, (1, 0)) # 출발지, 거리
+    heapq.heappush(queue, (0, 1)) # 가중치, 출발지
+    distance[1] = 0
     # 힙 순회
     while queue:
-        cur_node, cur_w = heapq.heappop(queue)
-        # 이미 최단 거리가 가중치보다 작으면 생략
+        # 우선순위 큐를 사용해서 cur_w가 가장 작은 노드가 먼저 나온다.
+        cur_w, cur_node = heapq.heappop(queue)
+        # 이미 최단 거리가 가중치보다 작으면(=이미 방문한 셈) 생략
         if distance[cur_node] < cur_w:
             continue
         # 다음 노드 경유
@@ -28,7 +31,7 @@ def solution(N, road, K):
             # 경유해서 가는 거리가 더 짧으면 distance 경신
             if next_w < distance[next_node]:
                 distance[next_node] = next_w
-                heapq.heappush(queue, (next_node, next_w))
+                heapq.heappush(queue, (next_w, next_node))
     
     # 최단 거리가 K 이하인 노드 개수
     answer = 0
