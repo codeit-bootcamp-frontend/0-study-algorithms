b02d452ebd27800b1e694fbe9369a3fa9c9f6e2c
drizzle96 <125791542+drizzle96@users.noreply.github.com>
2023-11-11 11:03:43 +0900

    [level 2] Title: [1차] 캐시, Time: 0.71 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -1,152 +1,33 @@
-class Node(object):
-    def __init__(self, data, prev = None, next = None):
-        self.data = data
-        self.prev = prev
-        self.next = next
+# deque에 remove 메소드가 있음을 간과했다.
 
-class DList(object):
-    def __init__(self):
-        self.head = Node(None)
-        self.tail = Node(None, self.head)
-        self.head.next = self.tail
-        self.size = 0
-    
-    def listSize(self):
-        return self.size
-    
-    def is_empty(self):
-        if self.size != 0:
-            return False
-        else:
-            return True
-    
-    def selectNode(self, idx):
-        if idx > self.size:
-            print("Overflow: Index Error")
-            return None
-        if idx == 0:
-            return self.head
-        if idx == self.size:
-            return self.tail
-        if idx <= self.size//2:
-            target = self.head
-            for _ in range(idx):
-                target = target.next
-            return target
-        else:
-            target = self.tail
-            for _ in range(self.size - idx):
-                target = target.prev
-            return target
-    
-    def appendleft(self, value):
-        if self.is_empty():
-            self.head = Node(value)
-            self.tail = Node(None, self.head)
-            self.head.next = self.tail
-        else:
-            tmp = self.head
-            self.head = Node(value, None, self.head)
-            tmp.prev = self.head
-        self.size += 1
-            
-    
-    def append(self, value):
-        if self.is_empty():
-            self.head = Node(value)
-            self.tail = Node(None, self.head)
-            self.head.next = self.tail
-        else:
-            tmp = self.tail.prev
-            newNode = Node(value, tmp, self.tail)
-            tmp.next = newNode
-            self.tail.prev = newNode
-        self.size += 1
-    
-    def insert(self, value, idx):
-        if self.is_empty():
-            self.head = Node(value)
-            self.tail = Node(None, self.head)
-            self.head.next = self.tail
-        else:
-            tmp = self.selectNode(idx)
-            if tmp == None:
-                return
-            if tmp == self.head:
-                self.appendleft(value)
-            elif tmp == self.tail:
-                self.append(value)
-            else:
-                tmp_prev = tmp.prev
-                newNode = Node(value, tmp_prev, tmp)
-                tmp_prev.next = newNode
-                tmp.prev = newNode
-        self.size += 1
-        
-    def findNode(self, data): #O(n)
-        crnt_node = self.head
-        while crnt_node is not None:
-            if crnt_node.data == data:
-                return crnt_node
-            crnt_node = crnt_node.next
-        return False
-    
-    def deleteNode(self, node):
-        if node == self.head:
-            self.head = node.next
-        elif node == self.tail:
-            self.tail = self.tail.prev
-        else:
-            node.prev.next = node.next
-            node.next.prev = node.prev
-        self.size -= 1
-    
-    def popleft(self):
-        if self.is_empty():
-            return
-        self.head = self.head.next
-        self.size -= 1
-    
-    def printlist(self):
-        target = self.head
-        while target != self.tail:
-            if target.next != self.tail:
-                print(target.data, '<=> ', end='')
-            else:
-                print(target.data)
-            target = target.next
+# dequq 메소드 정리
+# deque.append(item): item을 데크의 오른쪽 끝에 삽입한다.
+# deque.appendleft(item): item을 데크의 왼쪽 끝에 삽입한다.
+# deque.pop(): 데크의 오른쪽 끝 엘리먼트를 가져오는 동시에 데크에서 삭제한다.
+# deque.popleft(): 데크의 왼쪽 끝 엘리먼트를 가져오는 동시에 데크에서 삭제한다.
+# deque.extend(array): 주어진 배열(array)을 순환하면서 데크의 오른쪽에 추가한다.
+# deque.extendleft(array): 주어진 배열(array)을 순환하면서 데크의 왼쪽에 추가한다.
+# deque.remove(item): item을 데크에서 찾아 삭제한다.
+# deque.rotate(num): 데크를 num만큼 회전한다(양수면 오른쪽, 음수면 왼쪽).
+
+from collections import deque
 
 def solution(cacheSize, cities):
     if cacheSize == 0:
         return len(cities) * 5
     
     answer = 0
-    cache_dict = {}
-    d_list = DList()
+    q = deque()
     
-    for city_before_modify in cities:
-        city = city_before_modify.lower()
-        # 캐시에 있는지 확인해서 있으면 삭제
-        if city in cache_dict:
+    for city_original in cities:
+        city = city_original.lower()
+        if city in q:
+            q.remove(city)
             answer += 1
-            d_list.deleteNode(d_list.findNode(city))
-            cache_dict[city] -= 1
-            if cache_dict[city] == 0:
-                del cache_dict[city]
         else:
             answer += 5
-        # 캐시에 추가
-        d_list.append(city)
-        if city in cache_dict:
-            cache_dict[city] += 1
-        else:
-            cache_dict[city] = 1
-        # 길이 확인 후 넘치면 하나 제거
-        if d_list.listSize() > cacheSize:
-            head_data = d_list.head.data
-            cache_dict[head_data] -= 1
-            if cache_dict[head_data] == 0:
-                del cache_dict[head_data]
-            d_list.popleft()
+        q.append(city)
+        if len(q) > cacheSize:
+            q.popleft()
     
     return answer
